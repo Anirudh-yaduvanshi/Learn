c459d1e36c40ae580ac2808adac768e618486225
Anirudh yadav <yadavanirudh97.25@gmail.com>
2025-02-17 23:30:27 +0530

    bnaya vite se aur tailwind lgaya bss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button1 from "./Components/Button1";
+
+function App() {
+  
+
+  return (
+    <>
+      <Button1/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Button1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button1 = () => {
+  return <button className="rounded-2xl hover:bg-slate-500 hover:scale-90 bg-black text-white py-1 px-2 active:scale-100 m-1 " >Button 1</button>;
+};
+
+export default Button1;
